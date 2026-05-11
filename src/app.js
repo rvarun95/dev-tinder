b8479a308e9ee736347e50f7c34dfe542dfa622b
Varunkumar Rajendran <Varunkumar.Rajendran@wisetechglobal.com>
2026-05-10 20:29:11 -0400

    Basic Routes

--- src/app.js
+++ src/app.js
@@ -2,12 +2,20 @@ const express = require('express');
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send('Test from the server!');
+app.get("/test", (req, res) => {
+  res.send({ firstName: 'John', lastName: 'Doe' });
+});
+
+app.post("/test", (req, res) => {
+  res.send('Data stored successfully in Database!');
 });
 
 app.use("/hello", (req, res) => {
   res.send('Hello World from the server...');
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello from the server!');
 });
 
 app.listen(9999, () => {
